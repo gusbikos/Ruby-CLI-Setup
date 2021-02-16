99a187f5570e203144263ff8aea1cb0cc7ebccc0
gusbikos <gusbikos@gmail.com>
2021-02-16 12:26:48 -0500

    Migrations and Seed Data

--- app/sightseeing.rb
+++ app/sightseeing.rb
@@ -1,9 +1,9 @@
-class YourAppName
+class Sightseeing
   # here will be your CLI!
   # it is not an AR class so you need to add attr
 
   def run
-    # welcome
+    puts "welcome"
     # login_or_signup
     # wanna_see_favs?
     # some_method(some_argument)
